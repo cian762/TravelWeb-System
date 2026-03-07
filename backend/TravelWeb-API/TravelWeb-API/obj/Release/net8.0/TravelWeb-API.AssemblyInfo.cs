6615eddb023f609e7c9474465c50063e62f9ed33
cian762 <cian484848@gmail.com>
2026-03-07 22:19:30 +0800

    0307-2

--- backend/TravelWeb-API/TravelWeb-API/obj/Release/net8.0/TravelWeb-API.AssemblyInfo.cs
+++ backend/TravelWeb-API/TravelWeb-API/obj/Release/net8.0/TravelWeb-API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelWeb-API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf393db52632cb30ac0d3b6517040e483a243fcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelWeb-API")]
